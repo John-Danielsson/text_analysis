a97ac8a03400e08ac7741a9a723b2c449e587859
John Danielsson <johnaugustd@gmail.com>
2023-04-19 13:48:43 -0700

    resolve pep errors

--- file_to_txt.py
+++ file_to_txt.py
@@ -6,6 +6,7 @@ from os.path import basename, join
 from re import sub
 from docx import Document
 
+
 class FileToTXT:
 
     """Initializes the FileParser and calculates the text output
@@ -13,23 +14,23 @@ class FileToTXT:
     def __init__(self, filepath: str):
         self.text = []
         if filepath.endswith('.txt'):
-            self.text = self.parse_txt(filepath)
+            self.text = self.__parse_txt(filepath)
             self.filename = basename(filepath)[:-4]
         elif filepath.endswith('.epub'):
-            self.text = self.parse_epub(filepath)
+            self.text = self.__parse_epub(filepath)
             self.filename = basename(filepath)[:-5]
         elif filepath.startswith("http"):
-            self.text = self.parse_online_html(filepath)
+            self.text = self.__parse_online_html(filepath)
             cleaned_url = sub(r'https?://', '', filepath)
-            self.filename = sub(r'[^\w]', '_', cleaned_url)
+            self.filename = sub(r'\W', '_', cleaned_url)
         elif filepath.endswith('.html'):
-            self.text = self.parse_local_html(filepath)
+            self.text = self.__parse_local_html(filepath)
             self.filename = basename(filepath)[:-5]
         elif filepath.endswith('.pdf'):
-            self.text = self.parse_pdf(filepath)
+            self.text = self.__parse_pdf(filepath)
             self.filename = basename(filepath)[:-4]
         elif filepath.endswith('.docx'):
-            self.text = self.parse_docx(filepath)
+            self.text = self.__parse_docx(filepath)
             self.filename = basename(filepath)[:-5]
 
     """Returns the text representation of the given file."""
@@ -48,36 +49,35 @@ class FileToTXT:
         return self.__str__()
 
     """Parses text from a website. The URL must go to an HTML site."""
-    def parse_online_html(self, link: str) -> list:
+    def __parse_online_html(self, link: str) -> list:
         soup = BeautifulSoup(get(link).content, "html.parser")
         return [p.get_text() for p in soup.find_all("p")]
 
     """Parses text from a an HTML file"""
-    def parse_local_html(self, filepath: str) -> list:
+    def __parse_local_html(self, filepath: str) -> list:
         with open(filepath, 'r') as f:
             html = f.read()
         bs4_html = BeautifulSoup(html, "html.parser")
         return [p.get_text() for p in bs4_html.find_all("p")]
 
     """Parses <p> tags from the given HTML."""
-    @staticmethod
-    def html_to_str(html) -> str:
+    def __html_to_str(self, html) -> str:
         bs4_html = BeautifulSoup(html, "html.parser")
         return "\n".join(p.get_text() for p in bs4_html.find_all("p"))
 
     """Parses text from a .txt file."""
-    def parse_txt(self, filepath: str) -> list:
+    def __parse_txt(self, filepath: str) -> list:
         with open(file=filepath, mode="r") as file:
             return [file.read()]
 
     """Parses text from a .epub file."""
-    def parse_epub(self, filepath: str) -> list:
+    def __parse_epub(self, filepath: str) -> list:
         file = epub.read_epub(filepath)
         items = list(file.get_items_of_type(ITEM_DOCUMENT))
-        return [self.html_to_str(item.get_content()) for item in items]
+        return [self.__html_to_str(item.get_content()) for item in items]
 
     """Parses text from a .pdf file."""
-    def parse_pdf(self, filepath: str) -> list:
+    def __parse_pdf(self, filepath: str) -> list:
         with open(filepath, 'rb') as file:
             pdf = PdfFileReader(file)
             n_pages = pdf.getNumPages()
@@ -87,7 +87,7 @@ class FileToTXT:
             return result
 
     """Parses text from a .docx file."""
-    def parse_docx(self, filepath: str) -> list:
+    def __parse_docx(self, filepath: str) -> list:
         document = Document(filepath)
         return [p.text for p in document.paragraphs]
 
