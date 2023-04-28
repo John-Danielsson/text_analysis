df03ab9285b2561f219abaa732434733ce00d6a0
John Danielsson <johnaugustd@gmail.com>
2023-04-27 21:14:56 -0700

    new approach

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from os.path import join
+import os
 from tempfile import TemporaryDirectory
 from index import construct_index
 from file_parser import FileParser
@@ -13,6 +14,40 @@ davinci = "text-davinci-003"
 turbo = "gpt-3.5-turbo"
 gpt4 = "gpt-4"
 
+
+def primer(n_files: int) -> str:
+    return f"""
+All of the text after the 10 consecutive asterisks consists of text
+extracted from {n_files} files in .pdf, .txt, .html, .docx, or .epub format.
+The text of each file is formatted as follows:
+
+==========
+BEGIN FILE (file number)
+File Name: (file name)
+File Content:
+(text of file)
+END FILE (file number)
+==========
+
+**********
+
+"""
+
+
+def prompt_engineer(file_parser: FileParser, i: int) -> str:
+    return f"""
+
+==========
+BEGIN FILE {i}
+File Name: {file_parser.filename}
+File Content:
+{str(file_parser)}
+END FILE {i}
+==========
+
+"""
+
+
 @app.route("/")
 def index():
     print(f"json_index={json_index}")
@@ -23,29 +58,42 @@ def index():
 def upload():
     if request.method == "POST":
         files = request.files.getlist("files[]")
-        # texts = []
+        print(f"number of files={len(files)}")
         with TemporaryDirectory() as temp_dir:
-            for file in files:
-                file_path = join(temp_dir, file.filename)
-                file.save(file_path)
-                # txt = FileParser(file_path)
-                # texts.append(f"BEGIN {txt.filename}")
-                # texts.extend(txt.text)
-                # texts.append(f"END {txt.filename}")
-            global json_index
-            # json_index = GPTSimpleVectorIndex.load_from_disk("index.json")
-            json_index = construct_index(
-                directory_path=temp_dir,
-                model=turbo,
-                temperature=0.5
-            )
+            with TemporaryDirectory() as txt_dir:
+                txt_file_text = primer(len(files))
+                for i in range(len(files)):
+                    print(f"file name={files[i].filename}")
+                    file_path = join(temp_dir, files[i].filename)
+                    files[i].save(file_path)
+                    file_parser = FileParser(filepath=file_path)
+                    txt_file_text += prompt_engineer(file_parser, i+1)
+                with open(file=join(txt_dir, "data.txt"), mode="w", errors="ignore") as f:
+                    f.write(txt_file_text)
+                # with open(file=join(txt_dir, "data.txt"), mode="r", errors="ignore") as f:
+                #     print(f"\n\n\n\n\ntxt file text:\n{f.read()}")
+                # print("\n\nfor file in os.listdir(txt_dir):")
+                # for file in os.listdir(txt_dir):
+                #     print(f"    {file}")
+                # print(txt_file_text)
+                    # # texts.append(f"BEGIN {txt.filename}")
+                    # # texts.extend(txt.text)
+                    # # texts.append(f"END {txt.filename}")
+                global json_index
+                # json_index = GPTSimpleVectorIndex.load_from_disk("index.json")
+                json_index = construct_index(
+                    directory_path=txt_dir,
+                    model=turbo,
+                    temperature=0.5
+                )
+                print("index construction success")
         return jsonify({"status": "success"})
 
 
 @app.route("/query", methods=["POST"])
 def process_query():
     question = request.form.get("question")
-    print(f"\nquestion={question}")
+    print(f"\nquestion=\"{question}\"")
     print(f"\njson_index={json_index}")
     answer = json_index.query(question).response
     print(f"\nanswer=\"{answer}\"")
--- file_parser.py
+++ file_parser.py
@@ -1,13 +1,11 @@
 from ebooklib import epub, ITEM_DOCUMENT
 from bs4 import BeautifulSoup
 from requests import get
-from PyPDF2 import PdfFileReader
 from re import sub
 from datetime import datetime
 from os.path import basename
-
-# Use this class to convert a given file into a .txt file to
-# be read by LangChain's TextLoader? YES
+from docx import Document
+import fitz
 
 
 class FileParser:
@@ -17,38 +15,57 @@ class FileParser:
     def __init__(self, filepath: str):
         self.text = []
         self.filename = basename(filepath)
-        if filepath.endswith('.epub'):
+        if filepath.endswith(".epub"):
+            self.extension = ".epub"
             self.text = self.parse_epub(filepath)
         elif filepath.startswith("http"):
-            # if filepath.endswith('.pdf'):
-            # else:
+            self.extension = "http"
             self.text = self.parse_online_html(filepath)
-        elif filepath.endswith('.html'):
+        elif filepath.endswith(".html"):
+            self.extension = ".html"
             self.text = self.parse_local_html(filepath)
-        elif filepath.endswith('.pdf'):
+        elif filepath.endswith(".pdf"):
+            self.extension = ".pdf"
             self.text = self.parse_pdf(filepath)
+        elif filepath.endswith(".txt"):
+            self.extension = ".txt"
+            self.text = self.parse_txt(filepath)
+        elif filepath.endswith(".docx"):
+            self.extension = ".docx"
+            self.text = self.parse_docx(filepath)
 
     """Returns the text representation of the given file."""
     def __str__(self):
-        def clean_text(text: str) -> str:
-            encoded = text.encode(encoding="utf-8", errors="ignore")
-            return encoded.decode(encoding="utf-8", errors="ignore")
-        return "\n".join(clean_text(x) for x in self.text)
+        # def clean_text(text: str) -> str:
+        #     return text.encode(
+        #         encoding="utf-8",
+        #         errors="ignore"
+        #     ).decode(
+        #         encoding="utf-8",
+        #         errors="ignore"
+        #     )
+        # return "\n".join(clean_text(x) for x in self.text)
+        return "\n".join(p for p in self.text)
 
     """Parses text from a website."""
-    def parse_online_html(self, link: str) -> list:
+    @staticmethod
+    def parse_online_html(link: str) -> list:
         soup = BeautifulSoup(get(link).content, "html.parser")
         return [p.get_text() for p in soup.find_all("p")]
 
     """Parses text from a an HTML file"""
-    def parse_local_html(self, filepath: str) -> list:
+
+    @staticmethod
+    def parse_local_html(filepath: str) -> list:
         with open(filepath, 'r') as f:
             html = f.read()
         bs4_html = BeautifulSoup(html, "html.parser")
         return [p.get_text() for p in bs4_html.find_all("p")]
 
     """Parses <p> tags from the given HTML."""
-    def html_to_str(self, html) -> str:
+
+    @staticmethod
+    def html_to_str(html) -> str:
         bs4_html = BeautifulSoup(html, "html.parser")
         return "\n".join(p.get_text() for p in bs4_html.find_all("p"))
 
@@ -59,9 +76,22 @@ class FileParser:
         return [self.html_to_str(item.get_content()) for item in items]
 
     """Parses text from a .pdf file."""
-    def parse_pdf(self, filepath: str) -> list:
-        pages = PdfFileReader(open(filepath, 'rb')).pages
-        return [page.extract_text() for page in pages]
+    @staticmethod
+    def parse_pdf(filepath: str) -> list:
+        pdf = fitz.open(filepath)
+        return [page.get_text() for page in pdf]
+
+    """Parses text from a .txt file."""
+    @staticmethod
+    def parse_txt(filepath: str) -> list:
+        with open(file=filepath, mode="r", errors="ignore") as file:
+            return file.read().split("\n")
+
+    """Parses text from a .docx file."""
+    @staticmethod
+    def parse_docx(filepath: str) -> list:
+        document = Document(filepath)
+        return [p.text for p in document.paragraphs]
 
     def to_txt(self, filepath: str) -> None:
         timestamp = sub("[ -:.]", "_", datetime.now().__str__())
