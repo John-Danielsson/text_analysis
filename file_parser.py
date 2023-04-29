8493562fbb15a2bb7c3af072d4d43f057a5d688a
John Danielsson <johnaugustd@gmail.com>
2023-04-29 14:48:40 -0700

    clean up

--- file_parser.py
+++ file_parser.py
@@ -10,7 +10,6 @@ import fitz
 
 class FileParser:
 
-
     """Initializes the FileParser and calculates the text output
     based on the file extension"""
     def __init__(self, filepath: str):
@@ -37,27 +36,15 @@ class FileParser:
                 self.extension += "x"
             self.text = self.parse_docx_or_doc(filepath)
 
-
     """Returns the text representation of the given file."""
     def __str__(self):
-        # def clean_text(text: str) -> str:
-        #     return text.encode(
-        #         encoding="utf-8",
-        #         errors="ignore"
-        #     ).decode(
-        #         encoding="utf-8",
-        #         errors="ignore"
-        #     )
-        # return "\n".join(clean_text(x) for x in self.text)
         return "\n".join(p for p in self.text)
-
 
     """Parses text from a website."""
     @staticmethod
     def parse_online_html(link: str) -> list:
         soup = BeautifulSoup(get(link).content, "html.parser")
         return [p.get_text() for p in soup.find_all("p")]
-
 
     """Parses text from a an HTML file"""
     @staticmethod
@@ -67,13 +54,11 @@ class FileParser:
         bs4_html = BeautifulSoup(html, "html.parser")
         return [p.get_text() for p in bs4_html.find_all("p")]
 
-
     """Parses <p> tags from the given HTML."""
     @staticmethod
     def html_to_str(html) -> str:
         bs4_html = BeautifulSoup(html, "html.parser")
         return "\n".join(p.get_text() for p in bs4_html.find_all("p"))
-
 
     """Parses text from a .epub file."""
     def parse_epub(self, filepath: str) -> list:
@@ -81,13 +66,11 @@ class FileParser:
         items = list(file.get_items_of_type(ITEM_DOCUMENT))
         return [self.html_to_str(item.get_content()) for item in items]
 
-
     """Parses text from a .pdf file."""
     @staticmethod
     def parse_pdf(filepath: str) -> list:
         pdf = fitz.open(filepath)
         return [page.get_text() for page in pdf]
-
 
     """Parses text from a .txt file."""
     @staticmethod
@@ -95,13 +78,11 @@ class FileParser:
         with open(file=filepath, mode="r", errors="ignore") as file:
             return file.read().split("\n")
 
-
     """Parses text from a .docx file."""
     @staticmethod
     def parse_docx_or_doc(filepath: str) -> list:
         document = Document(filepath)
         return [p.text for p in document.paragraphs]
-
 
     """Saves the file to a .txt file in the given filepath."""
     def to_txt(self, filepath: str) -> None:
