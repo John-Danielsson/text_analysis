063c648515162ded044fc7ade56448c8404ed9ee
John Danielsson <johnaugustd@gmail.com>
2023-05-02 23:30:49 -0700

    add instructions

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from os.path import join
+from tempfile import TemporaryDirectory
+
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from os.path import join, exists as path_exists
-from tempfile import TemporaryDirectory
+from werkzeug.utils import secure_filename
+
 from file_parser import FileParser
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 from prompt_engineer import primer_str, prompt_engineer_str
@@ -15,21 +18,25 @@ gpt4 = "gpt-4"
 
 
 def primer(n_files: int) -> str:
+    """Returns the beginning of the prompt-engineered version of the file(s)."""
     return primer_str.format(n_files)
 
 
 def prompt_engineer(file: FileParser, i: int) -> str:
+    """Returns a prompt-engineered version of the file."""
     return prompt_engineer_str.format(i, file.filename, str(file), i)
 
 
 @app.route("/")
 def index():
+    """Displays the app's HTML/CSS/JS."""
     print(f"json_index={json_index}")
     return render_template("index.html")
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
+    """Handles files uploads in PDF, HTML, EPUB, DOC, DOCX, and TXT."""
     if request.method == "POST":
         files = request.files.getlist("files[]")
         print(f"number of files={len(files)}")
@@ -37,8 +44,9 @@ def upload():
             with TemporaryDirectory() as txt_dir:
                 txt_file_text = primer(len(files))
                 for i in range(len(files)):
-                    print(f"file name={files[i].filename}")
-                    file_path = join(temp_dir, files[i].filename)
+                    file_name = secure_filename(files[i].filename)
+                    print(f"file name={file_name}")
+                    file_path = join(temp_dir, file_name)
                     files[i].save(file_path)
                     file_parser = FileParser(filepath=file_path)
                     txt_file_text += prompt_engineer(file_parser, i+1)
@@ -53,18 +61,30 @@ def upload():
 
 @app.route("/query", methods=["POST"])
 def process_query():
+    """Handles user questions over the files uploaded. If no files have been,
+    uploaded, the app simply indicates that no files have been uploaded yet."""
     question = request.form.get("question")
     print(f"\nquestion=\"{question}\"")
     print(f"\njson_index={json_index}")
-    print("processing answer...")
-    try:
+    print("\nprocessing answer...")
+    if json_index is not None:
         answer = json_index.query(question).response
         print(f"\nanswer=\"{answer}\"")
-    except:
-        print("files not in index, alerting user")
-        message = "There are no files. Please upload at least 1 file to ask questions."
-        return jsonify({"status": "success", "response": message})
+    else:
+        print("\nindex not present, alerting user")
+        answer = "There are no files. Please upload at least 1 file to ask questions."
     return jsonify({"status": "success", "response": answer})
+
+
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
+
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    """Handles excessively large files."""
+    response = jsonify({"status": "error", "message": "Too much content uploaded. Maximum allowed size is 500MB."})
+    response.status_code = 413
+    return response
 
 
 if __name__ == "__main__":
---
+++ instructions.md
@@ -0,0 +1,23 @@
+# INSTRUCTIONS
+
+1) Navigate to the folder called ```text_analysis```.
+
+2) In the ```text_analysis``` folder, open the file ```app.py``` in PyCharm.
+
+3) In Pycharm, click on the green arrow in the upper right corner.
+
+<img src="./images/run_button.png" height="auto">
+
+4) Click on "Terminal" (it may already be open).
+
+<img src="./images/terminal.png" height="auto">
+
+5) Click on this link: [http://127.0.0.1:5000](http://127.0.0.1:5000) (you can also click in the terminal as shown below)
+
+<img src="./images/run_link.png" height="auto">
+
+6) Upload files and answer questions as desired.
+
+7) When you want to stop the app, you can either click the red square (just to the left of the green arrow) or close PyCharm.
+
+<img src="./images/stop_button.png" height="auto">
